efb24424ad85d345b489cff71d710293739608ad
JanKrzemien <jan.krzemien.6@gmail.com>
2024-03-30 12:16:38 +0100

    removed some shit

--- main.gd
+++ main.gd
@@ -1,19 +1,2 @@
 extends Node2D
 
-#Pierdolony popoiołek
-
-
-#dsadasdasdas
-#
-#
-#dsdasdasdas
-#
-#
-#dsadasdasda
-
-func mobbyn():
-	print("mobbyn mobbyn mobbyn mobbyn mobbyn.")
-
-func _ready():
-	print("chuj")
-	print('kys')
