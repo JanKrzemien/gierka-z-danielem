31da9904e3ac4c8ed79d55204d0c16c313b67b4f
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-03-30 11:56:53 +0100

    kys

--- main.gd
+++ main.gd
@@ -1,8 +1,3 @@
 extends Node2D
 
-@export var kokaina = 100
-
-func mobbyn(super_gwiazda_kurwa_ten):
-	print("mobbyn")
-	print("paluch to ziomal")
-	super_gwiazda_kurwa_ten.wciagaj_prochy_mordo()
+#Pierdolony Rockstar
