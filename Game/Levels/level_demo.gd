045fc787c314c57cfc942364841ad23b0d67e006
JohnnyArachnid <daniel.szarek@student.uj.edu.pl>
2024-04-20 11:21:21 +0200

    kys1

--- Game/Levels/level_demo.gd
+++ Game/Levels/level_demo.gd
@@ -1,6 +1,23 @@
 extends Node2D
 
+class_name GameManager
+
 signal lvl_finished
+
+signal toggle_game_paused(is_paused : bool)
+# Called when the node enters the scene tree for the first time.
+
+var game_paused : bool = false:
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = game_paused
+		emit_signal("toggle_game_paused", game_paused)
+		
+func _input(event : InputEvent):
+	if(event.is_action_pressed("ui_cancel")):
+		game_paused = !game_paused
 
 @onready var transition_button = $CanvasLayer/TransButton
 
