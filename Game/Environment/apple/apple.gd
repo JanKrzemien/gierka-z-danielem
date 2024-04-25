5775de44b9b6c6483e2a4c9eec78140e149ce18f
JanKrzemien <jan.krzemien.6@gmail.com>
2024-04-25 16:38:57 +0200

    apple heals on pick up

--- Game/Environment/apple/apple.gd
+++ Game/Environment/apple/apple.gd
@@ -2,19 +2,14 @@ extends Node2D
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+@export var bonus: float = 10
+
 func _ready():
 	animation_player.play("idle")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_area_2d_body_entered(body):
 	if (body.name == "Player"):
-		#zwiekszenie hp gracza
+		body.find_child("Damagable").heal(self, bonus)
 		var tween = create_tween()
 		tween.tween_property(self, "position", position + Vector2(0, -30), 0.5)
 		animation_player.play("collected")
